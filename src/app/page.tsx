cb59218ac0b22393c1b91f25c5be4883faeb50ff
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-10-09 23:17:41 -0300

    feat: dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="p-8 flex flex-col gap-6">
+    <div className="p-8 flex flex-col gap-6 dark:bg-zinc-950">
       <h1 className="flex gap-2 items-center font-bold  text-2xl md:text-5xl before:size-3 before:rounded-full before:bg-yellow-300 before:flex">
         Hello tailwind
       </h1>
