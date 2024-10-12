ed9a022c77624213da73a66cb2260b91368c2849
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-10-12 18:30:06 -0300

    feat: config eslint prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,26 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Masterizando Tailwind',
   description: 'Aprenda a usar o Tailwind CSS em projetos Next.js',
-};
+}
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
-        className={` 
-        ${inter.className}
-        antialiased dark:bg-zinc-950 text-white`}
+        className={` ${inter.className} text-white antialiased dark:bg-zinc-950`}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
-    <div className="p-8 flex flex-col gap-6 max-w-2xl">
+    <div className="flex max-w-2xl flex-col gap-6 p-8">
       <div className="max-w-[900px]">
-        <h1 className="flex gap-2 items-center font-bold  text-2xl md:text-5xl before:size-3 before:rounded-full before:bg-yellow-300 before:flex">
+        <h1 className="flex items-center gap-2 text-2xl font-bold before:flex before:size-3 before:rounded-full before:bg-yellow-300 md:text-5xl">
           Hello tailwind
         </h1>
 
-        <button className="bg-zinc-950 px-4 py-2 rounded w-24 hover:bg-zinc-950/80">
+        <button className="w-24 rounded bg-zinc-950 px-4 py-2 hover:bg-zinc-950/80">
           Sign in
         </button>
       </div>
     </div>
-  );
+  )
 }
