253c1056d4c82b08a63ad694bc1690394e770ad2
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-10-11 23:15:48 -0300

    feat: valores arbitarios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
     <div className="p-8 flex flex-col gap-6 max-w-2xl">
-      <h1 className="flex gap-2 items-center font-bold  text-2xl md:text-5xl before:size-3 before:rounded-full before:bg-yellow-300 before:flex">
-        Hello tailwind
-      </h1>
+      <div className="max-w-[900px]">
+        <h1 className="flex gap-2 items-center font-bold  text-2xl md:text-5xl before:size-3 before:rounded-full before:bg-yellow-300 before:flex">
+          Hello tailwind
+        </h1>
 
-      <button className="bg-zinc-950 px-4 py-2 rounded w-24 hover:bg-zinc-950/80">
-        Sign in
-      </button>
+        <button className="bg-zinc-950 px-4 py-2 rounded w-24 hover:bg-zinc-950/80">
+          Sign in
+        </button>
+      </div>
     </div>
   );
 }
