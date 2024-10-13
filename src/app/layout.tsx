e7268aac73bacba7b04d0bf93de4974db3e5c02a
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-10-13 20:24:38 -0300

    feat: sidebar logo e search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from '@/components/sidebar'
 import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -18,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={cn(inter.className)}>
-        <div className="grid-cols-app grid min-h-screen">
-          <aside className="border-r border-zinc-200 px-5 py-8">sidebar</aside>
+        <div className="grid min-h-screen grid-cols-app">
+          <Sidebar />
           <main className="px-4 pb-12 pt-8">{children}</main>
         </div>
       </body>
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,18 @@
+import { Search } from 'lucide-react'
+import { Logo } from './logo'
+
+export const Sidebar = () => {
+  return (
+    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        <Search className="size-5 text-zinc-500" />
+        <input
+          type="search"
+          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+          placeholder="Pesquisar..."
+        />
+      </div>
+    </aside>
+  )
+}
